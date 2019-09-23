aa4c13e6aab85526cc19e7e89c48e6d619929fa4
kevin <15010530879@139.com>
2019-09-23 16:21:36 +0800

    添加规范说明

--- README.md
+++ README.md
@@ -36,3 +36,11 @@ npm test
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
+# 规范
+1、class名称用_分隔
+2、js变量用驼峰定义
+3、公共组件写[/src/components]
+4、当前页面组件在当前文件夹下[/components]中
+5、页面中style标签需带有[scoped]，深层用 [/deep/]解决
+6、图片名称用_分隔，使用当前页面名称开头，如[home_bg.png]，如果是通用图片[common_]开头, icon用[icon_]开头
+
--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,23 +1,19 @@
 <template>
-  <div>
-    <home-swiper></home-swiper>
-    <p class="text">哈哈</p>
-    <p class="text1">哈哈</p>
-    <p class="text1 ignore">哈哈</p>
+  <div class="" style="background:rgba(0,0,0,0.8)">
+    
   </div>
 </template>
 <script>
-import HomeSwiper from './components/swiper'
 export default {
   name: 'Home',
-  components: {HomeSwiper: HomeSwiper}
+  components: {},
+  data(){
+    return {
+
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
-  .text{
-    font-size: 40px;
-  }
-  .ignore{
-    font-size:40px;
-  }
+  
 </style>
